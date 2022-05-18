0715dd0c11e0d00afd90e16723964bec3ff528ea
Deekshith M D <deekshithmogra@gmail.com>
2022-05-18 19:07:56 +0530

    fix: toast positition changed

--- src/components/Toast/Toast.jsx
+++ src/components/Toast/Toast.jsx
@@ -1,11 +1,13 @@
-import "./toast.css"
-import { ToastContainer } from "react-toastify"
-export const Toast=()=>{
-    return(
-        <ToastContainer
-        autoClose={2000}
-        closeOnClick
-        pauseOnFocusLoss={false}
-        pauseOnHover={false}/>  
-    )
-}
+import "./toast.css";
+import { ToastContainer } from "react-toastify";
+export const Toast = () => {
+  return (
+    <ToastContainer
+      className="toast-position"
+      autoClose={2000}
+      closeOnClick
+      pauseOnFocusLoss={false}
+      pauseOnHover={false}
+    />
+  );
+};
